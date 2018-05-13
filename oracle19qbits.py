2d7521db4a790935e2f543ba3b769177d8eacb03
Darío Clavijo <daedalus2027@gmail.com>
2018-05-13 01:25:41 -0300

    QPU execution

--- oracle19qbits.py
+++ oracle19qbits.py
@@ -5,6 +5,11 @@ from pyquil.quilatom import QubitPlaceholder
 from pyquil.quil import Program,address_qubits
 from pyquil.api import QVMConnection 
 from pyquil.gates import CNOT, H,X,Z,MEASURE
+from pyquil.api import get_devices, QPUConnection
+
+for device in get_devices():
+        if device.is_online():
+                    print('Device {} is online'.format(device.name))
 
 # this is the secret
 s = int(b'001100110011001100',2) # 1110
@@ -36,6 +41,8 @@ prog = address_qubits(prog)
 
 print(prog)
 
+print("Executing in the QVM")
+
 qvm = QVMConnection() 
 
 wf = qvm.wavefunction(prog)
@@ -46,3 +53,17 @@ results = qvm.run(prog, classical_addresses=class_readouts, trials=10)
 
 for r in results:
     print r
+
+print("Executing in the QPU")
+
+acorn = get_devices(as_dict=True)['19Q-Acorn']
+
+qpu = QPUConnection(acorn)
+# The device name as a string is also acceptable
+# qpu = QPUConnection('19Q-Acorn')
+
+results = qpu.run(prog, classical_addresses=class_readouts, trials=10)
+
+for r in results:
+    print r
+
